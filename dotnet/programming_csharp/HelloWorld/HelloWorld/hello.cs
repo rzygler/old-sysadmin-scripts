c497daa84c7b39a4825fb45a57320795e65a5e69
unknown <rzygler@.(none)>
2011-01-29 09:46:27 -0500

    adding first example

---
+++ dotnet/programming_csharp/HelloWorld/HelloWorld/hello.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class hello
+    {
+        static void Main(string[] args)
+        {
+            // Use the system console object
+            System.Console.WriteLine("Hello World");
+        }
+    }
+}
